c0fbb07c270da6cb3e5f3f3410549597ddd84ee3
draguarMatth <50259605+draguarMatth@users.noreply.github.com>
2022-01-10 17:28:21 +0100

    Debug OK

---
+++ NEE/NEE/Models/PageNavViewModel.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+
+
+
+namespace NEE.Models
+{
+    public class PageNavViewModel
+    {
+
+        
+
+        //public bool ShowPrevious => getPrevious();
+        //public bool ShowNext => getNext();
+
+        //public bool getPrevious()
+        //{
+        //    if (ViewData["returnUrl"] != null || ViewData["returnUrl"] != "")
+        //        return true;
+
+        //    return false;
+        //}
+
+        //public bool getNext()
+        //{
+        //    if (ViewData["nextnUrl"] != null || ViewData["nextnUrl"] != "")
+        //        return true;
+
+        //    return false;
+        //}
+
+
+    }
+}
